274828af0fdecb50cbd9b3f8afafad7beed4dd32
ferlolivares <molivaresm2100@alumno.ipn.mx>
2024-03-19 09:53:56 -0600

    miau

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-export const PORT = process.env.PORT || 2500
-export const MYSQLHOST = process.env.MYSQLHOST || 'localhost'
-export const MYSQLUSER = process.env.MYSQLUSER || 'root'
-export const MYSQL_ROOT_PASSWORD = process.env.MYSQL_ROOT_PASSWORD || '-RoDs0919_'
-export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'prueba'
-export const MYSQLPORT = process.env.MYSQLPORT || 3306
+export const PORT = process.env.PORT
+export const MYSQLHOST = process.env.MYSQLHOST
+export const MYSQLUSER = process.env.MYSQLUSER
+export const MYSQL_ROOT_PASSWORD = process.env.MYSQL_ROOT_PASSWORD
+export const MYSQLDATABASE = process.env.MYSQLDATABASE 
+export const MYSQLPORT = process.env.MYSQLPORT
